dc4a973d590176b42c98962ec5bd31ec49542265
barretodf <edsb1981@gmail.com>
2023-04-05 19:30:50 -0300

    Multiplicando 3 números

--- funcaoDeSoma.js
+++ funcaoDeSoma.js
@@ -12,3 +12,17 @@ function somarDoisNumeros(numero1, numero2) {
 }
 
 somarDoisNumeros(60, 40);
+//------------------------------------------------------------------
+
+function multiplicarDoisNumeros(a, b){
+    console.log(a * b)
+}
+
+multiplicarDoisNumeros(90, 90)
+
+//--------------------------------------------------------------
+function tresNumeros (a, b, c){
+    console.log('A multiplicação de 10 * 10 * 3 é:')
+    console.log(a * b * c)
+}
+tresNumeros(10, 10, 3)
